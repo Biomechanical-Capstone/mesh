fd95b670a84829763f6f077924fa77e6dbc96c50
jhunt10 <jxh074@uark.edu>
2017-02-03 15:31:36 -0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -11,5 +11,14 @@ public class Test {
 		BufferedInputStream is = new BufferedInputStream(new FileInputStream("test.stl"));
 		TriangleMesh mesh = reader.importStream(is);
 		is.close();
+		if(mesh != null)
+		{
+			STLWriter writer = new STLWriter();
+			OutputStream os = new FileOutputStream("out.stl");
+			writer.exportStream(mesh, os);
+			os.close();
+		}
+		else
+			System.out.println("MESH RETURNED NULL");
 	}
 }
